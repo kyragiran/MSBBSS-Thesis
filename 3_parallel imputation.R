bd53dcb59916b1d8ea1b0743d95952fbfcadb059
kyragiran <62139730+kyragiran@users.noreply.github.com>
2025-03-19 11:11:51 +0100

    Update 3_parallel imputation.R

--- 3_parallel imputation.R
+++ 3_parallel imputation.R
@@ -20,26 +20,19 @@ num_cores <- detectCores() - 1
 cl <- makeCluster(num_cores)
 registerDoParallel(cl)
 
-run_simulation <- function(sim_id, dataset_name, dataset) {
-  set.seed(123 + sim_id)
-  
-  imp_single <- do_single_imputation(dataset)
-  imp_multiple <- do_multiple_imputation(dataset)
-  imp_vae <- train_vae_and_impute(dataset, m = 5, epochs = 100, latent_dim = 2, lr = 0.001)
-  
-  true_coefs <- c(0, rep(0.2, 4))
-  
-  performance_single <- evaluate_vae_imputation(list(imp_single), true_coefs)
-  performance_multiple <- evaluate_vae_imputation(imp_multiple, true_coefs)
-  performance_vae <- evaluate_vae_imputation(imp_vae, true_coefs)
-  
-  return(data.frame(
+return(data.frame(
     sim_id = sim_id,
     dataset_name = dataset_name,
     RMSE_Single = mean(performance_single$rmse_values),
+    Bias_Single = mean(sapply(performance_single$bias_per_imputation, mean)),
+    CI_Coverage_Single = mean(unlist(performance_single$confidence_interval_coverage)),
     RMSE_Multiple = mean(performance_multiple$rmse_values),
-    RMSE_VAE = mean(performance_vae$rmse_values)
-  ))
+    Bias_Multiple = mean(sapply(performance_multiple$bias_per_imputation, mean)),
+    CI_Coverage_Multiple = mean(unlist(performance_multiple$confidence_interval_coverage)),
+    RMSE_VAE = mean(performance_vae$rmse_values),
+    Bias_VAE = mean(sapply(performance_vae$bias_per_imputation, mean)),
+    CI_Coverage_VAE = mean(unlist(performance_vae$confidence_interval_coverage))
+))
 }
 
 results <- foreach(sim_id = 1:num_simulations, .combine = rbind, .packages = c("mice", "MASS", "Matrix", "torch", "dplyr", "ggplot2", "Metrics")) %dopar% {
