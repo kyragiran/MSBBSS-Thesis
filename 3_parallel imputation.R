720c63aedad53652c26e85ecc4f9ccae9e018b39
kyragiran <62139730+kyragiran@users.noreply.github.com>
2025-04-03 09:25:55 +0200

    Update 3_parallel imputation.R

--- 3_parallel imputation.R
+++ 3_parallel imputation.R
@@ -1,16 +1,15 @@
-# 3_simulation.R
-
+# 3_simulation.R (final version)
 
 library(parallel)
 library(foreach)
 library(doParallel)
 library(torch)
 library(mice)
-library(progressr)
+library(dplyr)
+library(tibble)
+library(MASS)
 
 source("2_functions.R")
-
-missing_datasets <- readRDS("missing_datasets.RDS")
 
 num_simulations <- 50
 num_cores <- detectCores() - 1
@@ -19,44 +18,69 @@ registerDoParallel(cl)
 
 true_coefs <- c(0, rep(0.2, 4))
 
-run_simulation <- function(sim_id, dataset_name, dataset) {
+# Load missing datasets once 
+missing_datasets <- readRDS("missing_datasets.RDS")
+
+#  Run simulation for one condition 
+run_simulation <- function(sim_id, params) {
   set.seed(123 + sim_id)
+  prop <- params$prop
+  mech <- params$mech
+  dataset <- missing_datasets[[paste0(mech, "_", prop * 100, "%")]]
+
+  # Imputations
   imp_single <- do_single_imputation(dataset)
   imp_multiple <- do_multiple_imputation(dataset)
   imp_vae <- train_vae_and_impute(dataset, m = 5, epochs = 100, latent_dim = 2, lr = 0.001)
-  performance_single <- evaluate_single_imputation(imp_single, true_coefs)
-  performance_multiple <- evaluate_multiple_imputation(imp_multiple, true_coefs)
-  performance_vae <- evaluate_vae_imputation(imp_vae, true_coefs)
-  data.frame(
+
+  # Evaluations
+  eval_single <- evaluate_single_imputation(imp_single, true_coefs)
+  eval_multiple <- evaluate_multiple_imputation(imp_multiple, true_coefs)
+  eval_vae <- evaluate_vae_imputation(imp_vae, true_coefs)
+
+  # Combine results
+  list(
     sim_id = sim_id,
-    dataset_name = dataset_name,
-    RMSE_Single = performance_single$rmse_values,
-    Bias_Single = mean(performance_single$bias),
-    CI_Coverage_Single = mean(performance_single$confidence_interval_coverage),
-    RMSE_Multiple = performance_multiple$rmse_values,
-    Bias_Multiple = mean(performance_multiple$bias),
-    CI_Coverage_Multiple = mean(performance_multiple$confidence_interval_coverage),
-    RMSE_VAE = performance_vae$rmse_values,
-    Bias_VAE = mean(performance_vae$bias),
-    CI_Coverage_VAE = mean(performance_vae$confidence_interval_coverage)
+    missing_type = paste(mech, prop, sep = "_"),
+    single = eval_single %>% 
+      as_tibble(rownames = "term") %>% 
+      mutate(method = "single", sim_id = sim_id, missing_type = paste(mech, prop, sep = "_")),
+    multiple = eval_multiple %>% 
+      as_tibble(rownames = "term") %>% 
+      mutate(method = "multiple", sim_id = sim_id, missing_type = paste(mech, prop, sep = "_")),
+    vae = eval_vae %>% 
+      as_tibble(rownames = "term") %>% 
+      mutate(method = "vae", sim_id = sim_id, missing_type = paste(mech, prop, sep = "_"))
   )
 }
 
-handlers(global = TRUE)
-handlers("txtprogressbar")
+#  Simulation condition setup 
+sim.cond <- data.frame(mech = c(rep("MAR", 3), rep("MCAR", 3)), 
+                       prop = rep(c(0.1, 0.25, 0.5), 2)) %>% 
+  mutate(simname = as.factor(paste(mech, prop, sep = "_")))
 
-results <- with_progress({
-  p <- progressor(steps = num_simulations)
-  foreach(sim_id = 1:num_simulations, .combine = rbind, .packages = c("mice", "torch")) %dopar% {
-    source("2_functions.R")
-    res <- do.call(rbind, lapply(names(missing_datasets), function(name) {
-      run_simulation(sim_id, name, missing_datasets[[name]])
-    }))
-    p()
-    res
-  }
-})
+# Full Simulation 
+results <- foreach(sim_id = 1:num_simulations, .packages = c("mice", "torch", "dplyr", "tibble")) %dopar% {
+  source("2_functions.R")
+
+  sim_results <- lapply(levels(sim.cond$simname), function(name) {
+    params <- sim.cond[sim.cond$simname == name, 1:2]
+    run_simulation(sim_id, params)
+  })
+
+  sim_results
+}
 
 stopCluster(cl)
-write.csv(results, "simulation_results.csv", row.names = FALSE)
-print(summary(results))
+
+# ---- Flatten results to data frame ----
+data_long <- do.call(rbind, lapply(results, function(sim_list) {
+  do.call(rbind, lapply(sim_list, function(res) {
+    bind_rows(res$single, res$multiple, res$vae)
+  }))
+}))
+
+saveRDS(results, "simulation_results_nested_list.RDS")
+write.csv(data_long, "simulation_results_flat.csv", row.names = FALSE)
+
+print("Simulation complete. Full results saved.")
